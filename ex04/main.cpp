dd138c8584ed9814ed5c7b0d71034694f1f9790c
Thang Bui Quoc <tbuiloves42@gmail.com>
2025-04-27 18:09:46 +0200

    finished ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -10,10 +10,65 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
 
-#include "Template.hpp"
 
-int main(void)
+void replaceAndWrite(std::ifstream &inputFile, std::ofstream &outputFile, const std::string &searchString, const std::string &replaceString)
 {
+	std::string	line;
+	size_t		pos;
+	bool		found = false;
 
+	while (std::getline(inputFile, line))
+	{
+		while ((pos = line.find(searchString)) != std::string::npos)
+		{
+			line.erase(pos, searchString.length());
+			line.insert(pos, replaceString);
+			found = true;
+		}
+		outputFile << line << std::endl;
+	}
+	if (!found)
+		std::cout << "Warning: No occurrences of \"" << searchString << "\" were found in the file." << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Usage: " << " <filename> <search> <string>" << std::endl;
+		return 1;
+	}
+
+	std::string		inputFilename = argv[1];
+	std::string		searchString = argv[2];
+	std::string		replaceString = argv[3];
+
+	if (searchString.empty() || replaceString.empty())
+	{
+		std::cerr << "Error: search and replace strings cannot be empty." << std::endl;
+		return 2;
+	}
+	std::ifstream	inputFile(inputFilename.c_str()); //convert from String to const char * and calls constructor of ifstream
+
+	if (!inputFile.is_open())
+	{
+		std::cout << "Failed to open file " << argv[1] << "." << std::endl;
+		return 3;
+	}
+	std::string outputFilename = inputFilename + ".replace";
+	std::ofstream	outputFile(outputFilename.c_str());
+
+	if (!outputFile.is_open())
+	{
+		std::cout << "Failed to create file " << outputFilename << "." << std::endl;
+		return 4;
+	}
+	replaceAndWrite(inputFile, outputFile, searchString, replaceString);
+
+	inputFile.close();
+	outputFile.close();
+	return 0;
 }
